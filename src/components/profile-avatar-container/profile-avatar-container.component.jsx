38df878319262a9e8d62c47bbd27b4e596cf75d5
doan <nvdoan97@gmail.com>
2020-06-09 22:31:47 +0700

    user profile ui

--- src/components/profile-avatar-container/profile-avatar-container.component.jsx
+++ src/components/profile-avatar-container/profile-avatar-container.component.jsx
@@ -1,7 +1,104 @@
-import React from 'react';
+// imports the React Javascript Library
+import React from "react";
 
 import './profile-avatar-container.styles.scss';
 
-const ProfileAvatarContainer = () => <div></div>;
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
 
-export default ProfileAvatarContainer;
+//Tabs
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    width: 200,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  input: {
+    display: "none"
+  },
+  img: {
+    width: 200,
+    height: 256,
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
+  }
+});
+
+class ProfileAvatarContainer extends React.Component {
+  state = {
+    mainState: "uploaded",
+    imageUploaded: 0,
+    selectedFile: "avatar.jpg"
+  };
+
+  handleUploadClick = event => {
+    console.log();
+    var file = event.target.files[0];
+    const reader = new FileReader();
+    var url = reader.readAsDataURL(file);
+
+    reader.onloadend = function(e) {
+      this.setState({
+        selectedFile: [reader.result]
+      });
+    }.bind(this);
+    console.log(url); 
+
+    this.setState({
+      mainState: "uploaded",
+      selectedFile: event.target.files[0],
+      imageUploaded: 1
+    });
+  };
+
+  renderUploadedState() {
+    const { classes, theme } = this.props;
+    const { value } = this.state;
+
+    return (
+      <Grid container direction="column" alignItems="center">
+        <Grid item >
+          <img
+            width="100%"
+            className={classes.img}
+            src={this.state.selectedFile}
+          />
+        </Grid>
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" component="span">
+            Select Image
+            <input
+              accept="image/*"
+              className={classes.input}
+              id="contained-button-file"
+              multiple
+              type="file"
+              onChange={this.handleUploadClick}
+            />
+          </Button>
+        </label>
+      </Grid>
+    );
+  }
+
+  render() {
+    const { classes, theme } = this.props;
+    return (
+      <div className={classes.root}>
+        <Card className={this.props.cardName}>
+          {this.state.mainState == "uploaded" && this.renderUploadedState()}
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(ProfileAvatarContainer);
+
--- src/pages/user-profile/user-profile.component.jsx
+++ src/pages/user-profile/user-profile.component.jsx
@@ -2,15 +2,140 @@ import React from 'react';
 
 import ProfileInputForm from '../../components/profile-input-form/profile-input-form.component';
 import ProfileAvatarContainer from '../../components/profile-avatar-container/profile-avatar-container.component';
-
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 import './user-profile.styles.scss';
 
-const UserProfilePage = () => (
-  <div>
-    <h1>User Profile Page</h1>
-    <ProfileInputForm />
-    <ProfileAvatarContainer />
-  </div>
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import ButtonBase from "@material-ui/core/ButtonBase";
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    margin: 8
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "25ch"
+  },
+  image: {
+    width: 128,
+    height: 256
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
+  },
+  button: {
+    "& > *": {
+      margin: theme.spacing(4)
+    }
+  }
+}));
+
+
+const UserProfilePage = ({ currentUser}) => (
+<div className={useStyles.root}>
+<Grid container>
+  <Grid container sm direction="row" spacing={4} justify="center">
+    <Grid item xs={10}>
+      <h1>Profile</h1>
+    </Grid>
+    <Grid item xs={10}>
+      <TextField
+	id="outlined-full-width"
+	label="Username"
+	fullWidth
+	defaultValue="Default Value"
+	variant="outlined"
+      />
+    </Grid>
+    <Grid item xs={10}>
+      <TextField
+	id="outlined-full-width"
+	label="Email"
+	defaultValue="Default Value"
+	fullWidth
+	variant="outlined"
+      />
+    </Grid>
+
+    <Grid item xs={5}>
+      <TextField
+	id="outlined-full-width"
+	label="First Name"
+	defaultValue="Default Value"
+	fullWidth
+	variant="outlined"
+      />
+    </Grid>
+    <Grid item xs={5}>
+      <TextField
+	id="outlined-full-width"
+	label="Last Name"
+	defaultValue="Default Value"
+	fullWidth
+	variant="outlined"
+      />
+    </Grid>
+    <Grid item xs={5}>
+      <TextField
+	id="outlined-full-width"
+	label="Phone Number"
+	defaultValue="Default Value"
+	fullWidth
+	variant="outlined"
+      />
+    </Grid>
+
+    <Grid item xs={5}>
+      <TextField
+	id="date"
+	label="Birthday"
+	type="date"
+	defaultValue="2017-05-24"
+	fullWidth
+	InputLabelProps={{
+	  shrink: true
+	}}
+      />
+    </Grid>
+    <Grid item xs={10}>
+      <div className={useStyles.button}>
+	<Button variant="contained" color="primary">
+	  Update
+	</Button>
+	<Button variant="contained">Cancle</Button>
+      </div>
+    </Grid>
+  </Grid>
+
+  <Grid container xs={3} direction="column">
+    <Grid item xs={3}>
+      <h1>Avatar</h1>
+    </Grid>
+    <Grid item xs={3}>
+      <ProfileAvatarContainer cardName="Input Image" />
+    </Grid>
+
+  </Grid>
+</Grid>
+</div>
 );
 
+
+
+
+
 export default UserProfilePage;
+
+
